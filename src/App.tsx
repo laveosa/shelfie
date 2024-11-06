46de5e157dbcef1cee704d5d7421220292f24e6c
CheKorsar <chekorsar@gmail.com>
2024-11-06 16:03:53 +0200

    Application first time configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import cs from "@/App.module.scss";
+
+function App() {
+  return (
+    <div id={cs.ApplicationNameWrapper}>
+      <h1>Shelfie App</h1>
+    </div>
+  );
+}
+
+export default App;
