bc235e7da6b44bfafb65b2e157cea5d4bd1dd408
CheKorsar <chekorsar@gmail.com>
2024-11-12 14:40:04 +0200

    Add breadcrumb component from shadCn Update SheBreadcrumbs component

--- src/components/complex/she-breadcrumbs/SheBreadcrumbs.tsx
+++ src/components/complex/she-breadcrumbs/SheBreadcrumbs.tsx
@@ -1,17 +1,42 @@
+import { ChevronRight } from "lucide-react";
 import cs from "./SheBreadcrumbs.module.scss";
-import useBreadcrumbs from "@/utils/hooks/useBreadcrumbs.ts";
+import useBreadcrumbs from "@/utils/hooks/useBreadcrumbs";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 export default function SheBreadcrumbs() {
   const { crumbs } = useBreadcrumbs();
 
   return (
-    <div className={cs.sheBreadcrumbs}>
-      {crumbs.map((item, index) => (
-        <div key={index} className={cs.sheBreadcrumbsItem}>
-          {item}
-          <span>{`${--crumbs.length < index ? "/" : ""}`}</span>
-        </div>
-      ))}
-    </div>
+    <Breadcrumb className={cs.sheBreadcrumbs}>
+      <BreadcrumbList className={cs.BreadcrumbList}>
+        {crumbs.map((crumb: string, index: number) => {
+          const isLast: boolean = index === crumbs.length - 1;
+
+          return (
+            <BreadcrumbItem key={index}>
+              {isLast ? (
+                <BreadcrumbPage className={cs.BreadcrumbPage}>
+                  {crumb}
+                </BreadcrumbPage>
+              ) : (
+                <>
+                  <BreadcrumbLink href="#">{crumb}</BreadcrumbLink>
+                  <BreadcrumbSeparator>
+                    <ChevronRight className="h-4 w-4" />
+                  </BreadcrumbSeparator>
+                </>
+              )}
+            </BreadcrumbItem>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
   );
 }
