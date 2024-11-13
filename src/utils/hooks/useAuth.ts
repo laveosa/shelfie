486155f35de3b88c470071e01c55eab0e107881e
CheKorsar <chekorsar@gmail.com>
2024-11-13 19:27:35 +0200

    Add Auth logic

--- src/utils/hooks/useAuth.ts
+++ src/utils/hooks/useAuth.ts
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+
+import { RootState } from "@/state/store.ts";
+import { IAppSlice } from "@/const/interfaces/store-slices/IAppSlice.ts";
+import { StoreSliceEnum } from "@/const/enums/StoreSliceEnum.ts";
+
 export default function useAuth() {
-  // get User data from AppStoreSlice
-  // check if user is undefined and throw error message
-  return {}; //return user
+  const { user } = useSelector(
+    (state: RootState): IAppSlice => state[StoreSliceEnum.APP],
+  );
+
+  if (user === undefined) {
+    throw new Error("user not found in local environment");
+  }
+
+  return user;
 }
