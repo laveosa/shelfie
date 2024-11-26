c637ae260c66485b61e433b959a520dabc89d25a
CheKorsar <chekorsar@gmail.com>
2024-11-26 12:39:56 +0200

    Add SheSidebar component to App.tsx
    Add navbar to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,72 @@
 import { Outlet } from "react-router-dom";
 
 import "@/App.scss";
-import SheBreadcrumbs from "@/components/complex/she-breadcrumbs/SheBreadcrumbs.tsx";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar.tsx";
+import SheSidebar from "@/components/complex/she-sidebar/SheSidebar.tsx";
+import { Avatar } from "@/components/ui/avatar.tsx";
+import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Input } from "@/components/ui/input.tsx";
+import { BellDot, Search } from "lucide-react";
 
 function App() {
   return (
     <div id="ApplicationNameWrapper">
-      <SheBreadcrumbs />
-      <Outlet />
+      <SidebarProvider>
+        <SheSidebar />
+        <div
+          style={{ display: "flex", flexDirection: "column", width: "100%" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              borderBottom: "1px solid black",
+              width: "100%",
+            }}
+          >
+            <SidebarTrigger
+              style={{
+                margin: "15px",
+                marginBottom: "10px",
+                width: "15px",
+                height: "15px",
+              }}
+            />
+            <div className="relative h-10 w-full">
+              <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500 z-10" />
+              <Input
+                type="search"
+                placeholder="Search"
+                style={{
+                  width: "200px",
+                  height: "25px",
+                  marginTop: "9px",
+                  backgroundColor: "lightgray",
+                  padding: "2px 3px 2px 35px ",
+                }}
+                // className="pl-10 pr-3 py-2 text-sm" // Add additional styling as needed
+                value=""
+              />
+            </div>
+            <BellDot style={{ color: "gray", margin: "10px" }} />
+            <Avatar
+              style={{
+                width: "30px",
+                height: "30px",
+                marginTop: "5px",
+                borderRadius: "10px",
+              }}
+            >
+              <AvatarImage
+                src="https://github.com/shadcn.png"
+                alt="@shadcn"
+                style={{ zIndex: 999 }}
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+          <Outlet />
+        </div>
+      </SidebarProvider>
     </div>
   );
 }
--- src/components/complex/she-sidebar/SheSidebar.tsx
+++ src/components/complex/she-sidebar/SheSidebar.tsx
@@ -1,3 +1,192 @@
+import cs from "./SheSidebar.module.scss";
+import logo from "@/assets/icons/TNF_logo.svg";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar.tsx";
+import {
+  ChevronDown,
+  IceCreamBowl,
+  IdCard,
+  LayoutDashboard,
+  LifeBuoy,
+  MessageCircle,
+  ReceiptEuro,
+  Settings,
+  Shirt,
+  Users,
+  Video,
+} from "lucide-react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@radix-ui/react-collapsible";
+import { Avatar } from "@/components/ui/avatar.tsx";
+import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+
+const projects = [
+  {
+    title: "First",
+    icon: IceCreamBowl,
+  },
+  {
+    title: "Second",
+    icon: IdCard,
+  },
+];
+
+const activityItems = [
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Products",
+    url: "/products",
+    icon: Shirt,
+  },
+  {
+    title: "Messenger",
+    url: "/messenger",
+    icon: MessageCircle,
+  },
+  {
+    title: "Orders",
+    url: "/orders",
+    icon: ReceiptEuro,
+  },
+  {
+    title: "Transmissions",
+    url: "/transmissions",
+    icon: Video,
+  },
+];
+
+const usersItems = [
+  {
+    title: "Users",
+    url: "/users",
+    icon: Users,
+  },
+];
+
+const setupItems = [
+  {
+    title: "Setting",
+    url: "/settings",
+    icon: Settings,
+  },
+  {
+    title: "Support",
+    url: "/support",
+    icon: LifeBuoy,
+  },
+];
+
 export default function SheSidebar() {
-  return <div>SheSidebar</div>;
+  return (
+    <Sidebar className={cs.SheSidebar} collapsible="icon">
+      <Collapsible className="group/collapsible">
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                <Avatar style={{ borderRadius: "10px" }}>
+                  <AvatarImage
+                    src={logo}
+                    alt="@shadcn"
+                    style={{ zIndex: 999 }}
+                  ></AvatarImage>
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingLeft: "10px",
+                  }}
+                >
+                  <p>To NIE Fabryka</p>
+                  <p style={{ fontSize: "8px" }}>Subscription Active</p>
+                </div>
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {projects.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <a href={"https://www.google.com"}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Collapsible>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>ACTIVITY</SidebarGroupLabel>
+          <SidebarMenu>
+            {activityItems.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>USERS</SidebarGroupLabel>
+          <SidebarMenu>
+            {usersItems.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>SETUP</SidebarGroupLabel>
+          <SidebarMenu>
+            {setupItems.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
 }
