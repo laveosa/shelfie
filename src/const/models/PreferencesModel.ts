ca09d620ce5c2c96c227389e2dad0c2694bebb60
CheKorsar <chekorsar@gmail.com>
2025-02-03 09:42:08 +0200

    (ColumnsViewOptions) Add logic

--- src/const/models/PreferencesModel.ts
+++ src/const/models/PreferencesModel.ts
@@ -1,20 +1,45 @@
+export interface ProductReferencesModel {
+  columns?: {
+    id?: boolean;
+    image?: boolean;
+    code?: boolean;
+    productName?: boolean;
+    category?: boolean;
+    brand?: boolean;
+    barcode?: boolean;
+    status?: boolean;
+    salePrice?: boolean;
+    variantCount?: boolean;
+    stock?: boolean;
+  };
+}
+
 export interface PreferencesModel {
-  globalPreferences: {};
+  globalPreferences?: any;
+  viewsReferences?: {
+    productReferences?: ProductReferencesModel;
+  };
+}
+
+//------------------------------------------------ DEFAULT MODEL
+
+export const PreferencesModelDefault: PreferencesModel = {
+  globalPreferences: {},
   viewsReferences: {
     productReferences: {
       columns: {
-        id: boolean;
-        image: boolean;
-        code: boolean;
-        productName: boolean;
-        category: boolean;
-        brand: boolean;
-        barcode: boolean;
-        status: boolean;
-        salePrice: boolean;
-        variantCount: boolean;
-        stock: boolean;
-      };
-    };
-  };
-}
+        id: false,
+        image: false,
+        code: false,
+        productName: false,
+        category: false,
+        brand: false,
+        barcode: false,
+        status: false,
+        salePrice: false,
+        variantCount: false,
+        stock: false,
+      },
+    },
+  },
+};
