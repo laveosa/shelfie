a586c849b3659776c8670df07ece674247159ee2
CheKorsar <chekorsar@gmail.com>
2025-02-05 14:59:13 +0200

    (GridDataTable) refactored to work through props

--- src/const/interfaces/complex-components/IGridHeader.ts
+++ src/const/interfaces/complex-components/IGridHeader.ts
@@ -1,17 +1,6 @@
 import { Table } from "@tanstack/react-table";
-import { GridModel } from "@/const/models/GridModel.ts";
 import { PropsWithChildren } from "react";
-import { GridRequestModel } from "@/const/models/GridRequestModel.ts";
-import { GridSortingModel } from "@/const/models/GridSortingModel.ts";
 
 export interface IGridHeader<TData> extends PropsWithChildren {
-  gridModel: GridModel;
   table?: Table<TData>;
-  sortingItems?: GridSortingModel[];
-  showPagination?: boolean;
-  showSorting?: boolean;
-  showColumnsViewOptions?: boolean;
-  showSearch?: boolean;
-  onSearchHandle?: () => void;
-  onGridRequestChange?: (updates: GridRequestModel) => void;
 }
