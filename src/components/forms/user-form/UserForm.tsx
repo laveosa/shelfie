b9ce1cf9dab42245092a64c0fcbbd446803320d4
Nik <laveosa@yahoo.com>
2025-04-24 15:48:51 +0300

    (UserForm) Update form logo path

--- src/components/forms/user-form/UserForm.tsx
+++ src/components/forms/user-form/UserForm.tsx
@@ -14,7 +14,7 @@ import { IUserForm } from "@/const/interfaces/forms/IUserForm.ts";
 import { ISheSelectItem } from "@/const/interfaces/primitive-components/ISheSelect.ts";
 
 import { User } from "lucide-react";
-import logo from "@/assets/icons/TNF_logo.svg?react";
+import TNFLogoIcon from "@/assets/icons/TNF_logo.svg?react";
 import { DirectionEnum } from "@/const/enums/DirectionEnum.ts";
 
 export default function UserForm<T>({
@@ -67,7 +67,7 @@ export default function UserForm<T>({
       <SheForm<T>
         form={form}
         defaultValues={UserModelDefault}
-        image={logo}
+        image={TNFLogoIcon}
         formPosition={DirectionEnum.CENTER}
         title="User Form"
         view={ComponentViewEnum.CARD}
