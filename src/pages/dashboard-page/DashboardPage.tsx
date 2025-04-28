eb9238b6340ae2f96601db413a071d1085a3f58e
Nik <laveosa@yahoo.com>
2025-04-28 14:32:23 +0300

    TEST COMMIT

--- src/pages/dashboard-page/DashboardPage.tsx
+++ src/pages/dashboard-page/DashboardPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import cs from "./DashboardPage.module.scss";
 import useDashboardPageService from "@/pages/dashboard-page/useDashboardPageService.ts";
+import SheTextArea from "@/components/primitive/she-textarea/SheTextarea.tsx";
 
 export function DashboardPage() {
   const service = useDashboardPageService();
@@ -9,6 +10,8 @@ export function DashboardPage() {
   return (
     <div id={cs["DashboardPage"]}>
       <h1>Dashboard</h1>
+
+      <SheTextArea label="Textarea" />
     </div>
   );
 }
