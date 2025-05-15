6370f54986629b709290f6e652c5cfcae74ae4e1
Nik <laveosa@yahoo.com>
2025-05-15 19:24:53 +0300

    build fixes

--- src/const/interfaces/primitive-components/ISheTimePickerInput.ts
+++ src/const/interfaces/primitive-components/ISheTimePickerInput.ts
@@ -1,11 +1,7 @@
-import { ComponentPropsWithRef } from "react";
-
 import { Period, TimePickerType } from "@/utils/helpers/time-picker-helper.ts";
 import { ISheInput } from "@/const/interfaces/primitive-components/ISheInput.ts";
 
-export interface ISheTimePickerInput
-  extends ISheInput,
-    ComponentPropsWithRef<"input"> {
+export interface ISheTimePickerInput extends ISheInput {
   picker: TimePickerType;
   date: Date | undefined;
   period?: Period;
