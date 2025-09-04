845ff8b3c69c96118222a966ec6113588cb78100
Nik <laveosa@yahoo.com>
2025-09-04 15:46:35 +0300

    Dashboard page clean up

--- src/pages/dashboard-page/DashboardPage.tsx
+++ src/pages/dashboard-page/DashboardPage.tsx
@@ -2,9 +2,6 @@ import React from "react";
 
 import cs from "./DashboardPage.module.scss";
 import useDashboardPageService from "@/pages/dashboard-page/useDashboardPageService.ts";
-import UserForm, {
-  userFormExampleModel,
-} from "@/components/forms/examples/user-form/UserForm.tsx";
 
 export function DashboardPage() {
   const service = useDashboardPageService();
@@ -13,29 +10,12 @@ export function DashboardPage() {
 
   // ================================================================== EVENT HANDLERS
 
-  function onAction(event, model?) {
-    console.log("EVENT: ", event);
-    console.log("MODEL: ", model);
-  }
-
   // ================================================================== LOGIC
 
   // ================================================================== LAYOUT
   return (
     <div id={cs["DashboardPage"]}>
       <h1>Dashboard</h1>
-      <br />
-      <br />
-
-      <UserForm
-        data={userFormExampleModel}
-        notDisabledSubmit
-        onSubmit={onAction}
-        onCancel={onAction}
-      />
-
-      <br />
-      <br />
     </div>
   );
 }
