c546b7d6981b81f5439c298cf6be9006307ba1ce
CheKorsar <chekorsar@gmail.com>
2025-09-18 09:55:36 +0300

    Bugs fixing

--- src/components/complex/custom-cards/manage-variants-card/ManageVariantsCard.tsx
+++ src/components/complex/custom-cards/manage-variants-card/ManageVariantsCard.tsx
@@ -5,15 +5,20 @@ import React, { Fragment } from "react";
 
 import {
   DataWithId,
-  DndGridDataTable,
+  DndGridDataTable
 } from "@/components/complex/grid/dnd-grid/DndGrid.tsx";
-import SheProductCard from "@/components/complex/she-product-card/SheProductCard.tsx";
+import SheProductCard
+  from "@/components/complex/she-product-card/SheProductCard.tsx";
 import cs from "./ManageVariantsCard.module.scss";
 import SheButton from "@/components/primitive/she-button/SheButton.tsx";
 import { TraitModel } from "@/const/models/TraitModel.ts";
-import { IManageVariantsCard } from "@/const/interfaces/complex-components/custom-cards/IManageVariantsCard.ts";
+import {
+  IManageVariantsCard
+} from "@/const/interfaces/complex-components/custom-cards/IManageVariantsCard.ts";
 import { Separator } from "@/components/ui/separator.tsx";
-import { ManageVariantsGridColumns } from "@/components/complex/grid/custom-grids/manage-variants-grid/ManageVariantsGridColumns.tsx";
+import {
+  ManageVariantsGridColumns
+} from "@/components/complex/grid/custom-grids/manage-variants-grid/ManageVariantsGridColumns.tsx";
 
 export default function ManageVariantsCard({
   isLoading,
@@ -121,7 +126,7 @@ export default function ManageVariantsCard({
         <Separator />
         <div className={cs.buttonBlock}>
           <span className="she-title">{t("SectionTitles.Variant")}</span>
-          {traits.length > 0 && (
+          {traits?.length > 0 && (
             <SheButton
               icon={Plus}
               variant="secondary"
