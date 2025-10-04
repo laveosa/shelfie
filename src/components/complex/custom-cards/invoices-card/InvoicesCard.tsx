9fd7b19fb680488bc4cf0c2fc0f80edf64c3cc0d
Nik <laveosa@yahoo.com>
2025-10-04 16:27:05 +0300

    (InvoiceCard) Update component logic and layout, replace SheProductCard component to SheCard

--- src/components/complex/custom-cards/invoices-card/InvoicesCard.tsx
+++ src/components/complex/custom-cards/invoices-card/InvoicesCard.tsx
@@ -1,15 +1,13 @@
-import { ColumnDef } from "@tanstack/react-table";
 import React from "react";
-import { useTranslation } from "react-i18next";
 
-import SheProductCard from "@/components/complex/she-product-card/SheProductCard.tsx";
 import cs from "./InvoicesCard.module.scss";
+import SheCard from "@/components/complex/she-card/SheCard.tsx";
 import { SheGrid } from "@/components/complex/grid/SheGrid.tsx";
 import { SheFileUploader } from "@/components/complex/she-file-uploader/SheFileUploader.tsx";
+import { InvoicesGridColumns } from "@/components/complex/grid/custom-grids/invoices-grid/InvoicesGridColumns.tsx";
+import useAppTranslation from "@/utils/hooks/useAppTranslation.ts";
 import { UploadPhotoModel } from "@/const/models/UploadPhotoModel.ts";
 import { IInvoiceCard } from "@/const/interfaces/complex-components/custom-cards/IInvoicesCard.ts";
-import { InvoicesGridColumns } from "@/components/complex/grid/custom-grids/invoices-grid/InvoicesGridColumns.tsx";
-import { DataWithId } from "@/const/interfaces/complex-components/ISheGrid.ts";
 
 export default function InvoicesCard({
   isLoading,
@@ -19,8 +17,10 @@ export default function InvoicesCard({
   contextId,
   onAction,
 }: IInvoiceCard) {
-  const { t } = useTranslation();
+  // ==================================================================== UTILITIES
+  const { translate } = useAppTranslation();
 
+  // ==================================================================== EVENT HANDLERS
   function onGridAction(
     actionType: string,
     _rowId?: string,
@@ -41,57 +41,53 @@ export default function InvoicesCard({
     onAction("deleteInvoice", data);
   }
 
+  // ==================================================================== LAYOUT
   return (
-    <div className={cs.invoicesCard}>
-      <SheProductCard
-        loading={isLoading}
-        title={t("CardTitles.Invoices")}
-        className={cs.productPhotosCard}
-      >
-        <div className={cs.invoicesCardContent}>
-          <SheFileUploader
-            isLoading={isImageUploaderLoading}
-            contextName="invoiceDocument"
-            viewMode="file"
-            contextId={contextId}
-            acceptedFileTypes={{
-              "application/pdf": [".pdf"],
-              "application/msword": [".doc"],
-              "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-                [".docx"],
-              "application/vnd.ms-excel": [".xls"],
-              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-                [".xlsx"],
-              "image/jpeg": [".jpg", ".jpeg"],
-              "image/png": [".png"],
-            }}
-            onUpload={(uploadModel: UploadPhotoModel) =>
-              onAction("uploadInvoice", uploadModel)
-            }
-          />
-          <div className={cs.manageInvoices}>
-            <div className={`${cs.manageInvoicesTitle} she-title`}>
-              {t("CardTitles.ManageInvoices")}
-            </div>
-            <div className={cs.manageInvoicesGrid}>
-              <SheGrid
-                isLoading={isGridLoading}
-                className={cs.invoicesGrid}
-                showHeader={false}
-                columns={
-                  InvoicesGridColumns(
-                    onGridAction,
-                    onDelete,
-                  ) as ColumnDef<DataWithId>[]
-                }
-                data={data}
-                skeletonQuantity={10}
-                customMessage={t("InvoiceMessages.NoFilesUploaded")}
-              />
-            </div>
+    <SheCard
+      className={cs.invoicesCard}
+      title="Invoices"
+      titleTransKey="CardTitles.Invoices"
+      isLoading={isLoading}
+    >
+      <div className={cs.invoicesCardContent}>
+        <SheFileUploader
+          isLoading={isImageUploaderLoading}
+          contextName="invoiceDocument"
+          viewMode="file"
+          contextId={contextId}
+          acceptedFileTypes={{
+            "application/pdf": [".pdf"],
+            "application/msword": [".doc"],
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+              [".docx"],
+            "application/vnd.ms-excel": [".xls"],
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+              [".xlsx"],
+            "image/jpeg": [".jpg", ".jpeg"],
+            "image/png": [".png"],
+          }}
+          onUpload={(uploadModel: UploadPhotoModel) =>
+            onAction("uploadInvoice", uploadModel)
+          }
+        />
+        <div className={cs.manageInvoices}>
+          <div className={`${cs.manageInvoicesTitle} she-title`}>
+            {translate("CardTitles.ManageInvoices")}
+          </div>
+          <div className={cs.manageInvoicesGrid}>
+            <SheGrid
+              isLoading={isGridLoading}
+              className={cs.invoicesGrid}
+              showHeader={false}
+              columns={InvoicesGridColumns(onGridAction, onDelete)}
+              data={data}
+              skeletonQuantity={10}
+              customMessage="There are no files uploaded yet"
+              customMessageTransKey="InvoiceMessages.NoFilesUploaded"
+            />
           </div>
         </div>
-      </SheProductCard>
-    </div>
+      </div>
+    </SheCard>
   );
 }
