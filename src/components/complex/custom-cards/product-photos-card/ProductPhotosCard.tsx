40cd881b756555f29d7e314d38beaf8e426db731
Nik <laveosa@yahoo.com>
2025-10-06 13:05:37 +0300

    (ProductPhotosCard) Update component logic and layout, replace SheProductCard component to SheCard

--- src/components/complex/custom-cards/product-photos-card/ProductPhotosCard.tsx
+++ src/components/complex/custom-cards/product-photos-card/ProductPhotosCard.tsx
@@ -1,15 +1,13 @@
-import { ColumnDef } from "@tanstack/react-table";
-import { useTranslation } from "react-i18next";
 import React from "react";
 
-import SheProductCard from "@/components/complex/she-product-card/SheProductCard.tsx";
 import cs from "./ProductPhotosCard.module.scss";
+import SheCard from "@/components/complex/she-card/SheCard.tsx";
 import { SheGrid } from "@/components/complex/grid/SheGrid.tsx";
 import { SheFileUploader } from "@/components/complex/she-file-uploader/SheFileUploader.tsx";
+import { ProductPhotosGridColumns } from "@/components/complex/grid/custom-grids/variant-photos-grid/ProductPhotosGridColumns.tsx";
+import useAppTranslation from "@/utils/hooks/useAppTranslation.ts";
 import { UploadPhotoModel } from "@/const/models/UploadPhotoModel.ts";
 import { IProductPhotosCard } from "@/const/interfaces/complex-components/custom-cards/IProductPhotosCard.ts";
-import { ProductPhotosGridColumns } from "@/components/complex/grid/custom-grids/variant-photos-grid/ProductPhotosGridColumns.tsx";
-import { DataWithId } from "@/const/interfaces/complex-components/ISheGrid.ts";
 
 export default function ProductPhotosCard({
   isLoading,
@@ -21,8 +19,10 @@ export default function ProductPhotosCard({
   showCloseButton,
   onAction,
 }: IProductPhotosCard) {
-  const { t } = useTranslation();
+  // ==================================================================== UTILITIES
+  const { translate } = useAppTranslation();
 
+  // ==================================================================== EVENT HANDLER
   function handleAction(actionType: string, payload?: any): any {
     switch (actionType) {
       case "upload":
@@ -63,52 +63,49 @@ export default function ProductPhotosCard({
     }
   }
 
+  // ==================================================================== LAYOUT
   return (
-    <div className={cs.productPhotosCard}>
-      <SheProductCard
-        loading={isLoading}
-        title={t("CardTitles.ProductPhotos")}
-        minWidth="500px"
-        showCloseButton={showCloseButton}
-        onSecondaryButtonClick={() => onAction("closeProductPhotsCard")}
-        className={cs.productPhotosCard}
-      >
-        <div className={cs.productPhotosCardContent}>
-          <SheFileUploader
-            isLoading={isImageUploaderLoading}
-            contextName="product"
-            contextId={contextId}
-            onUpload={(uploadModel: UploadPhotoModel) =>
-              handleAction("upload", uploadModel)
-            }
-          />
-          <div className={cs.managePhotos}>
-            <div className={`${cs.managePhotosTitle} she-title`}>
-              {t("CardTitles.ManagePhotos")}
-            </div>
-            <div className={cs.managePhotosGrid}>
-              <SheGrid
-                isLoading={isGridLoading}
-                className={cs.photosGrid}
-                enableDnd={true}
-                showHeader={false}
-                columns={
-                  ProductPhotosGridColumns(
-                    onGridAction,
-                  ) as ColumnDef<DataWithId>[]
-                }
-                data={data}
-                skeletonQuantity={productCounter?.gallery}
-                cellPadding="5px 10px"
-                customMessage={t("ProductMessages.NoPhotos")}
-                onNewItemPosition={(newIndex, activeItem, oldIndex) =>
-                  handleAction("dnd", { newIndex, activeItem, oldIndex })
-                }
-              />
-            </div>
+    <SheCard
+      className={cs.productPhotosCard}
+      title="Product Photos"
+      titleTransKey="CardTitles.ProductPhotos"
+      minWidth="500px"
+      showCloseButton={showCloseButton}
+      isLoading={isLoading}
+      onSecondaryButtonClick={() => onAction("closeProductPhotsCard")}
+    >
+      <div className={cs.productPhotosCardContent}>
+        <SheFileUploader
+          isLoading={isImageUploaderLoading}
+          contextName="product"
+          contextId={contextId}
+          onUpload={(uploadModel: UploadPhotoModel) =>
+            handleAction("upload", uploadModel)
+          }
+        />
+        <div className={cs.managePhotos}>
+          <span className={`${cs.managePhotosTitle} she-title`}>
+            {translate("CardTitles.ManagePhotos")}
+          </span>
+          <div className={cs.managePhotosGrid}>
+            <SheGrid
+              className={cs.photosGrid}
+              isLoading={isGridLoading}
+              enableDnd={true}
+              showHeader={false}
+              columns={ProductPhotosGridColumns(onGridAction)}
+              data={data}
+              skeletonQuantity={productCounter?.gallery}
+              cellPadding="5px 10px"
+              customMessage="PRODUCT HAS NO PHOTO"
+              customMessageTransKey="ProductMessages.NoPhotos"
+              onNewItemPosition={(newIndex, activeItem, oldIndex) =>
+                handleAction("dnd", { newIndex, activeItem, oldIndex })
+              }
+            />
           </div>
         </div>
-      </SheProductCard>
-    </div>
+      </div>
+    </SheCard>
   );
 }
