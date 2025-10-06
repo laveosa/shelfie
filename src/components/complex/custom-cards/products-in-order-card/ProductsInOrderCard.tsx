c15257bb8b0113555dd6f1d3cd66b60bf9578503
Nik <laveosa@yahoo.com>
2025-10-06 13:31:24 +0300

    (ProductInOrderCard) Update component logic and layout, replace SheProductCard component to SheCard

--- src/components/complex/custom-cards/products-in-order-card/ProductsInOrderCard.tsx
+++ src/components/complex/custom-cards/products-in-order-card/ProductsInOrderCard.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { ColumnDef } from "@tanstack/react-table";
-import { Plus } from "lucide-react";
-import { useTranslation } from "react-i18next";
 
-import { SheGrid } from "@/components/complex/grid/SheGrid.tsx";
-import SheProductCard from "@/components/complex/she-product-card/SheProductCard.tsx";
+import { Plus } from "lucide-react";
+
 import cs from "./ProductsInOrderCard.module.scss";
-import { IProductsInOrderCard } from "@/const/interfaces/complex-components/custom-cards/IProductsInOrderCard.ts";
 import SheButton from "@/components/primitive/she-button/SheButton.tsx";
 import ProductsInOrderForm from "@/components/forms/products-in-order-form/PrductsInOrderForm.tsx";
+import SheCard from "@/components/complex/she-card/SheCard.tsx";
+import { SheGrid } from "@/components/complex/grid/SheGrid.tsx";
 import { ProductsInOrderGridColumns } from "@/components/complex/grid/custom-grids/products-in-order-grid/ProductsInOrderGridColums.tsx";
-import { DataWithId } from "@/const/interfaces/complex-components/ISheGrid.ts";
+import useAppTranslation from "@/utils/hooks/useAppTranslation.ts";
+import { IProductsInOrderCard } from "@/const/interfaces/complex-components/custom-cards/IProductsInOrderCard.ts";
 
 export default function ProductsInOrderCard({
   isLoading,
@@ -18,13 +17,13 @@ export default function ProductsInOrderCard({
   stockActions,
   onAction,
 }: IProductsInOrderCard) {
-  const { t } = useTranslation();
-  const createEmptyStockAction = () => ({
-    brutto: null,
-    unitsAmount: null,
-  });
+  // ==================================================================== STATE MANAGEMENT
   const [stockActionsData, setStockActionsData] = useState([]);
 
+  // ==================================================================== UTILITIES
+  const { translate } = useAppTranslation();
+
+  // ==================================================================== SIDE EFFECTS
   useEffect(() => {
     if (!stockActions || stockActions.length === 0) return;
 
@@ -35,12 +34,18 @@ export default function ProductsInOrderCard({
         expandableRows:
           stockAction.variantStockActions?.length > 0
             ? stockAction.variantStockActions
-            : [createEmptyStockAction()],
+            : [
+                {
+                  brutto: null,
+                  unitsAmount: null,
+                },
+              ],
       })),
     );
   }, [stockActions]);
 
-  const renderExpandedContent = (row) => {
+  // ==================================================================== PRIVATE
+  function renderExpandedContent(row) {
     return (
       <ProductsInOrderForm
         data={row.original}
@@ -50,25 +55,28 @@ export default function ProductsInOrderCard({
         onDelete={() => onAction("removeStockAction", row)}
       />
     );
-  };
+  }
 
+  // ==================================================================== LAYOUT
   return (
-    <SheProductCard
-      loading={isLoading}
-      title={t("CardTitles.ProductsInOrder")}
-      width="485px"
-      minWidth="485px"
+    <SheCard
       className={cs.productsInOrderCard}
+      title="Products in order"
+      titleTransKey="CardTitles.ProductsInOrder"
+      maxWidth="485px"
+      minWidth="485px"
+      isLoading={isLoading}
     >
       <div className={cs.productsInOrderCardContent}>
         <div className={cs.addProductBlock}>
           <span className="she-text">
-            {t("OrderMessages.NoOrderCompleteWithoutProduct")}
+            {translate("OrderMessages.NoOrderCompleteWithoutProduct")}
           </span>
           <SheButton
-            variant="secondary"
-            value={t("OrderActions.AddProduct")}
+            value="Add Product"
+            valueTransKey="OrderActions.AddProduct"
             icon={Plus}
+            variant="secondary"
             onClick={() => onAction("addProduct")}
           />
         </div>
@@ -76,11 +84,9 @@ export default function ProductsInOrderCard({
           key={stockActionsData.length}
           isLoading={isGridLoading}
           showHeader={false}
-          columns={
-            ProductsInOrderGridColumns({
-              onAction,
-            }) as ColumnDef<DataWithId>[]
-          }
+          columns={ProductsInOrderGridColumns({
+            onAction,
+          })}
           data={stockActionsData}
           skeletonQuantity={10}
           enableExpansion={true}
@@ -90,12 +96,12 @@ export default function ProductsInOrderCard({
           <span className="she-title"></span>
           <div className={cs.productsSummary}>
             <span className="she-text">
-              {t("OrderForm.Labels.ProductsTotal")}
+              {translate("OrderForm.Labels.ProductsTotal")}
             </span>
             <span className="she-text"></span>
           </div>
         </div>
       </div>
-    </SheProductCard>
+    </SheCard>
   );
 }
