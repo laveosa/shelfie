a34e57b2e5930727fafff985e7b8c6fc3eeb73ad
Nik <laveosa@yahoo.com>
2025-10-27 14:55:21 +0200

    (Navigation) Update - scroll to top page after page get loaded

--- src/components/complex/she-app-context-main-wrapper/SheAppContextMainWrapper.tsx
+++ src/components/complex/she-app-context-main-wrapper/SheAppContextMainWrapper.tsx
@@ -1,3 +1,6 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import cs from "./SheAppContextMainWrapper.module.scss";
 import { useSidebar } from "@/components/ui/sidebar.tsx";
 import { ISheAppContextMainWrapper } from "@/const/interfaces/complex-components/ISheAppContextMainWrapper.ts";
@@ -6,6 +9,11 @@ export default function SheAppContextMainWrapper({
   children,
 }: ISheAppContextMainWrapper) {
   const { open, isMobile } = useSidebar();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <main
