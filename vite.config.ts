84e527fdea84555fffb721f9d98b8e95efe93dc5
Nik <laveosa@yahoo.com>
2025-10-28 18:28:28 +0200

    fix vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/shelfie/',
   server: {
     port: 8000,
   },
