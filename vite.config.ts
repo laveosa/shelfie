de294e0e979cb0441421c537e7b922dd5c70b1d0
Nik <laveosa@yahoo.com>
2025-10-30 17:13:45 +0200

    DEV branch initialization

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/shelfie/",
+  base: "/shelfie",
   plugins: [react(), svgr()],
   server: {
     port: 8000,
