2e58bb06c3d5019f52a3cccb0d8b66acc0e46ceb
Nik <laveosa@yahoo.com>
2025-11-10 05:57:21 +0200

    (DictionaryApiService) Implement api stub logic

---
+++ db/controllers/dictionary-controller.ts
@@ -0,0 +1,41 @@
+import { BaseController } from "./base-controller";
+
+import countyCodeModel from "../static-collections/Dictionaries/country-code.json";
+import sortingOptionsForGrid from "../static-collections/Dictionaries/sorting-options-for-grid.json";
+import listOfTypesOfTraits from "../static-collections/Dictionaries/list-of-types-traits.json";
+import taxesList from "../static-collections/Dictionaries/taxes-list.json";
+import currenciesList from "../static-collections/Dictionaries/currencies-list.json";
+import languagesList from "../static-collections/Dictionaries/languages-list.json";
+import deliveryServicesList from "../static-collections/Dictionaries/delivery-services-list.json";
+
+export class DictionaryController extends BaseController {
+  public static getCountryCode() {
+    return this.staticDataApiHandler(countyCodeModel);
+  }
+
+  public static getSortingOptionsForGrid() {
+    return this.staticDataApiHandler(
+      this.createGridDefaultModel(sortingOptionsForGrid),
+    );
+  }
+
+  public static getListOfTypesOfTraits() {
+    return this.staticDataApiHandler(listOfTypesOfTraits);
+  }
+
+  public static getTaxesList() {
+    return this.staticDataApiHandler(taxesList);
+  }
+
+  public static getCurrenciesList() {
+    return this.staticDataApiHandler(currenciesList);
+  }
+
+  public static getLanguagesList() {
+    return this.staticDataApiHandler(languagesList);
+  }
+
+  public static getDeliveryServicesList() {
+    return this.staticDataApiHandler(deliveryServicesList);
+  }
+}
