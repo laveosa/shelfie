e2ae42221167e17c83a1ac908da7d08e84c140e5
Nik <laveosa@yahoo.com>
2025-11-10 20:59:02 +0200

    (PurchasesApiService) Implement api stub logic

--- src/utils/services/api/PurchasesApiService.ts
+++ src/utils/services/api/PurchasesApiService.ts
@@ -1,5 +1,6 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 
+import { PurchasesController as controller } from "db/controllers/purchases-controller.ts";
 import { ApiServiceNameEnum } from "@/const/enums/ApiServiceNameEnum.ts";
 import { ApiUrlEnum } from "@/const/enums/ApiUrlEnum.ts";
 import { ApiConfigurationService } from "@/utils/services/api/ApiConfigurationService.ts";
@@ -9,283 +10,226 @@ import { MarginItemModel } from "@/const/models/MarginItemModel.ts";
 import { MarginRuleModel } from "@/const/models/MarginRuleModel.ts";
 
 const apiConfig = new ApiConfigurationService(ApiUrlEnum.PURCHASES_BASE_URL);
+type ControllerType = typeof controller;
 
 export const PurchasesApiService = createApi({
   reducerPath: ApiServiceNameEnum.PURCHASES,
-  baseQuery: apiConfig.baseQueryWithInterceptors,
+  baseQuery: async () => ({ data: undefined }),
+  // baseQuery: apiConfig.baseQueryWithInterceptors,
   tagTypes: [ApiServiceNameEnum.PURCHASES],
   endpoints: (builder) => ({
-    getListOfPurchasesForGrid: apiConfig.createMutation<
+    getListOfPurchasesForGrid: builder.mutation<
       GridRequestModel,
       GridRequestModel
-    >(builder, {
-      query: (model?: GridRequestModel) => ({
-        url: `${ApiUrlEnum.PURCHASES}/list`,
-        method: "POST",
-        body: JSON.stringify(model),
-      }),
-    }),
-    getListOfPurchaseProductsForGrid: apiConfig.createMutation<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "getListOfPurchasesForGrid",
+        controller,
+      ),
+    ),
+    getListOfPurchaseProductsForGrid: builder.mutation<
       GridRequestModel,
       { id: number; model: GridRequestModel }
-    >(builder, {
-      query: ({ id, model }) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${id}${ApiUrlEnum.STOCK_ACTIONS}`,
-        method: "POST",
-        body: model,
-      }),
-    }),
-    getPurchaseDetails: apiConfig.createQuery<PurchaseModel, number>(builder, {
-      query: (id: number) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${id}`,
-      }),
-    }),
-    createPurchaseForSupplier: apiConfig.createMutation<any, any>(builder, {
-      query: (model?: any) => ({
-        url: `${ApiUrlEnum.PURCHASES}`,
-        method: "POST",
-        body: JSON.stringify(model),
-      }),
-    }),
-    updatePurchaseForSupplier: apiConfig.createMutation<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "getListOfPurchaseProductsForGrid",
+        controller,
+      ),
+    ),
+    getPurchaseDetails: builder.query<PurchaseModel, number>(
+      apiConfig.getStaticData<ControllerType>("getPurchaseDetails", controller),
+    ),
+    createPurchaseForSupplier: builder.mutation<any, any>(
+      apiConfig.getStaticData<ControllerType>(
+        "createPurchaseForSupplier",
+        controller,
+      ),
+    ),
+    updatePurchaseForSupplier: builder.mutation<
       any,
       {
         purchaseId: number;
         model: any;
       }
-    >(builder, {
-      query: ({ purchaseId, model }) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}`,
-        method: "PATCH",
-        body: JSON.stringify(model),
-      }),
-    }),
-    getPurchaseCounters: apiConfig.createQuery<any, number>(builder, {
-      query: (purchaseId: number) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/report`,
-      }),
-    }),
-    addVariantToPurchaseProducts: apiConfig.createMutation<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "updatePurchaseForSupplier",
+        controller,
+      ),
+    ),
+    getPurchaseCounters: builder.query<any, number>(
+      apiConfig.getStaticData<ControllerType>(
+        "getPurchaseCounters",
+        controller,
+      ),
+    ),
+    addVariantToPurchaseProducts: builder.mutation<
       any,
       {
         id: number;
         model: any;
       }
-    >(builder, {
-      query: ({ id, model }) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${id}${ApiUrlEnum.VARIANTS}`,
-        method: "POST",
-        body: JSON.stringify(model),
-      }),
-    }),
-    updatePurchaseProduct: apiConfig.createMutation<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "addVariantToPurchaseProducts",
+        controller,
+      ),
+    ),
+    updatePurchaseProduct: builder.mutation<
       any,
       {
         id: number;
         model: any;
       }
-    >(builder, {
-      query: ({ id, model }) => ({
-        url: `${ApiUrlEnum.PURCHASES}${ApiUrlEnum.STOCK_ACTIONS}/${id}`,
-        method: "PATCH",
-        body: JSON.stringify(model),
-      }),
-    }),
-    getPurchaseProductVariants: apiConfig.createQuery<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "updatePurchaseProduct",
+        controller,
+      ),
+    ),
+    getPurchaseProductVariants: builder.query<
       any,
       {
         purchaseId: number;
         productId: number;
       }
-    >(builder, {
-      query: ({ purchaseId, productId }) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}${ApiUrlEnum.PRODUCTS}/${productId}`,
-      }),
-    }),
-    deleteStockAction: apiConfig.createMutation<void, number>(builder, {
-      query: (stockActionId: number) => ({
-        url: `${ApiUrlEnum.PURCHASES}${ApiUrlEnum.STOCK_ACTIONS}/${stockActionId}`,
-        method: "DELETE",
-      }),
-    }),
-    getPurchaseSummary: apiConfig.createQuery<any, number>(builder, {
-      query: (purchaseId) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/fiscal-summary`,
-      }),
-    }),
-    getMarginsListForGrid: apiConfig.createMutation<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "getPurchaseProductVariants",
+        controller,
+      ),
+    ),
+    deleteStockAction: builder.mutation<void, number>(
+      apiConfig.getStaticData<ControllerType>("deleteStockAction", controller),
+    ),
+    getPurchaseSummary: builder.query<any, number>(
+      apiConfig.getStaticData<ControllerType>("getPurchaseSummary", controller),
+    ),
+    getMarginsListForGrid: builder.mutation<
       any,
       {
         model: GridRequestModel;
       }
-    >(builder, {
-      query: (model) => ({
-        url: `${ApiUrlEnum.MARGIN}/list`,
-        method: "POST",
-        body: JSON.stringify(model),
-      }),
-    }),
-    getAllMargins: apiConfig.createQuery<any, void>(builder, {
-      query: () => ({
-        url: `${ApiUrlEnum.MARGIN}/all`,
-      }),
-    }),
-    getMarginForPurchase: apiConfig.createQuery<any, number>(builder, {
-      query: (purchaseId) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/margin-rules`,
-      }),
-    }),
-    getMarginDetails: apiConfig.createQuery<any, number>(builder, {
-      query: (marginId) => ({
-        url: `${ApiUrlEnum.MARGIN}/${marginId}`,
-      }),
-    }),
-    createMargin: apiConfig.createMutation<any, { model: any }>(builder, {
-      query: (model) => ({
-        url: `${ApiUrlEnum.MARGIN}`,
-        method: "POST",
-        body: JSON.stringify(model),
-      }),
-    }),
-    updateMargin: apiConfig.createMutation<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "getMarginsListForGrid",
+        controller,
+      ),
+    ),
+    getAllMargins: builder.query<any, void>(
+      apiConfig.getStaticData<ControllerType>("getAllMargins", controller),
+    ),
+    getMarginForPurchase: builder.query<any, number>(
+      apiConfig.getStaticData<ControllerType>(
+        "getMarginForPurchase",
+        controller,
+      ),
+    ),
+    getMarginDetails: builder.query<any, number>(
+      apiConfig.getStaticData<ControllerType>("getMarginDetails", controller),
+    ),
+    createMargin: builder.mutation<any, { model: any }>(
+      apiConfig.getStaticData<ControllerType>("createMargin", controller),
+    ),
+    updateMargin: builder.mutation<
       any,
       {
         marginId: number;
         model: any;
       }
-    >(builder, {
-      query: ({ marginId, model }) => ({
-        url: `${ApiUrlEnum.MARGIN}/${marginId}`,
-        method: "PATCH",
-        body: JSON.stringify(model),
-      }),
-    }),
-    createMarginRules: apiConfig.createMutation<
+    >(apiConfig.getStaticData<ControllerType>("updateMargin", controller)),
+    createMarginRules: builder.mutation<
       any,
       {
         marginId: number;
         model: any;
       }
-    >(builder, {
-      query: ({ marginId, model }) => ({
-        url: `${ApiUrlEnum.MARGIN}/${marginId}/rules`,
-        method: "PATCH",
-        body: JSON.stringify(model),
-      }),
-    }),
-    deleteMargin: apiConfig.createMutation<void, number>(builder, {
-      query: (marginId: number) => ({
-        url: `${ApiUrlEnum.MARGIN}/${marginId}`,
-        method: "DELETE",
-      }),
-    }),
-    restoreMargin: apiConfig.createMutation<any, number>(builder, {
-      query: (marginId) => ({
-        url: `${ApiUrlEnum.MARGIN}/${marginId}/undelete`,
-        method: "PATCH",
-      }),
-    }),
-    connectMarginToPurchase: apiConfig.createMutation<
+    >(apiConfig.getStaticData<ControllerType>("createMarginRules", controller)),
+    deleteMargin: builder.mutation<void, number>(
+      apiConfig.getStaticData<ControllerType>("deleteMargin", controller),
+    ),
+    restoreMargin: builder.mutation<any, number>(
+      apiConfig.getStaticData<ControllerType>("restoreMargin", controller),
+    ),
+    connectMarginToPurchase: builder.mutation<
       any,
       {
         purchaseId: number;
         marginId: number;
       }
-    >(builder, {
-      query: ({ purchaseId, marginId }) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/margin-rules/${marginId}`,
-        method: "PATCH",
-      }),
-    }),
-    detachMargin: apiConfig.createMutation<void, number>(builder, {
-      query: (purchaseId: number) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/margin-rules`,
-        method: "DELETE",
-      }),
-    }),
-    updateMarginRulesForPurchase: apiConfig.createMutation<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "connectMarginToPurchase",
+        controller,
+      ),
+    ),
+    detachMargin: builder.mutation<void, number>(
+      apiConfig.getStaticData<ControllerType>("detachMargin", controller),
+    ),
+    updateMarginRulesForPurchase: builder.mutation<
       any,
       {
         purchaseId: number;
         model: MarginRuleModel;
       }
-    >(builder, {
-      query: ({ purchaseId, model }) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/margin-rules`,
-        method: "PATCH",
-        body: JSON.stringify(model),
-      }),
-    }),
-    getMarginItemsListForGrid: apiConfig.createMutation<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "updateMarginRulesForPurchase",
+        controller,
+      ),
+    ),
+    getMarginItemsListForGrid: builder.mutation<
       any,
       {
         purchaseId: number;
         model: GridRequestModel;
       }
-    >(builder, {
-      query: ({ purchaseId, model }) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/margin-items/list`,
-        method: "POST",
-        body: JSON.stringify(model),
-      }),
-    }),
-    getInvoicesForGrid: apiConfig.createMutation<any, number>(builder, {
-      query: (purchaseId) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/invoices`,
-        method: "POST",
-        body: {},
-      }),
-    }),
-    restoreMarginRuleToDefault: apiConfig.createMutation<any, number>(builder, {
-      query: (purchaseId) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/margin-rules/restore`,
-        method: "PATCH",
-      }),
-    }),
-    updateMarginItem: apiConfig.createMutation<
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "getMarginItemsListForGrid",
+        controller,
+      ),
+    ),
+    getInvoicesForGrid: builder.mutation<any, number>(
+      apiConfig.getStaticData<ControllerType>("getInvoicesForGrid", controller),
+    ),
+    restoreMarginRuleToDefault: builder.mutation<any, number>(
+      apiConfig.getStaticData<ControllerType>(
+        "restoreMarginRuleToDefault",
+        controller,
+      ),
+    ),
+    updateMarginItem: builder.mutation<
       any,
       {
         marginItemId: number;
         model: MarginItemModel;
       }
-    >(builder, {
-      query: ({ marginItemId, model }) => ({
-        url: `${ApiUrlEnum.MARGIN_ITEMS}/${marginItemId}/manage`,
-        method: "PATCH",
-        body: JSON.stringify(model),
-      }),
-    }),
-    applyMarginItem: apiConfig.createMutation<any, number>(builder, {
-      query: (marginItemId) => ({
-        url: `${ApiUrlEnum.MARGIN_ITEMS}/${marginItemId}/apply`,
-        method: "POST",
-      }),
-    }),
-    applyVisibleMarginItems: apiConfig.createMutation<
+    >(apiConfig.getStaticData<ControllerType>("updateMarginItem", controller)),
+    applyMarginItem: builder.mutation<any, number>(
+      apiConfig.getStaticData<ControllerType>("applyMarginItem", controller),
+    ),
+    applyVisibleMarginItems: builder.mutation<
       any,
       {
         purchaseId: number;
         model: GridRequestModel;
       }
-    >(builder, {
-      query: ({ purchaseId, model }) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/margin-items/apply`,
-        method: "POST",
-        body: JSON.stringify(model),
-      }),
-    }),
-    applyAllMarginItems: apiConfig.createMutation<any, number>(builder, {
-      query: (purchaseId) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}/margin-items/apply-all`,
-        method: "POST",
-      }),
-    }),
-    deletePurchase: apiConfig.createMutation<void, number>(builder, {
-      query: (purchaseId: number) => ({
-        url: `${ApiUrlEnum.PURCHASES}/${purchaseId}`,
-        method: "DELETE",
-      }),
-    }),
+    >(
+      apiConfig.getStaticData<ControllerType>(
+        "applyVisibleMarginItems",
+        controller,
+      ),
+    ),
+    applyAllMarginItems: builder.mutation<any, number>(
+      apiConfig.getStaticData<ControllerType>(
+        "applyAllMarginItems",
+        controller,
+      ),
+    ),
+    deletePurchase: builder.mutation<void, number>(
+      apiConfig.getStaticData<ControllerType>("deletePurchase", controller),
+    ),
   }),
 });
 
