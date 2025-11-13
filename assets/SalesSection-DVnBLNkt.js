5544a99769380e68c83de8c832d7f343e473ebd2
Nik <laveosa@yahoo.com>
2025-11-13 15:24:45 +0200

    Updates

---
+++ assets/SalesSection-DVnBLNkt.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-BBiTGp2l.js";const e={};function n(){return t.jsx("div",{id:e.SalesSection,children:t.jsx(s,{})})}export{n as SalesSection};
---
+++ assets/TransmissionsSection-BanhMaA_.js
@@ -0,0 +1,1 @@
+import{j as s,O as i}from"./index-BBiTGp2l.js";const n={};function o(){return s.jsx("div",{id:n.TransmissionsSection,children:s.jsx(i,{})})}export{o as TransmissionsSection};
