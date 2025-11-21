e5f2603ad9008adc2e5edb480114ee81f812a311
Nik <laveosa@yahoo.com>
2025-11-21 15:16:30 +0200

    Update phone format in stub data

--- src/components/complex/custom-cards/manage-product-card/ManageProductCard.tsx
+++ src/components/complex/custom-cards/manage-product-card/ManageProductCard.tsx
@@ -33,8 +33,6 @@ export default function ManageProductCard({
   productTraits,
   onAction,
 }: IManageProductCard) {
-  console.log("VARIANTS: ", variants);
-
   // ==================================================================== STATE MANAGEMENT
   const [variantsData, setVariantsData] = useState([]);
 
